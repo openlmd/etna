630df314aeba0f36a6f5f34d5aa7f31de0335e3a
Jorge Rodríguez-Araújo <jorge.rodriguez@aimen.es>
2015-11-12 09:10:56 +0100

    Some refactoring works.
    Former-commit-id: a8612d83180939a7b5e6f0d057b2e3f5637da62d
    Former-commit-id: 30435bab74699f3004b13764a9cb64af15d91fbe

--- etna_calib/scripts/sub_viewer.py
+++ etna_calib/scripts/sub_viewer.py
@@ -22,33 +22,33 @@ path = rospack.get_path('etna_calib')
 class ImageViewer():
     def __init__(self):
         rospy.init_node('viewer', anonymous=True)
-        
+
         image_topic = rospy.get_param('~image', '/camera/image')
         # Pattern parameters
         pattern_rows = rospy.get_param('~pattern_rows', 7)
         pattern_cols = rospy.get_param('~pattern_cols', 8)
         pattern_size = rospy.get_param('~pattern_size', 10)
-        # 
+
         config_file = rospy.get_param('~config', 'profile3d.yaml')
-        
+
         rospy.Subscriber(image_topic, Image, self.callback, queue_size=10)
         rospy.on_shutdown(self.on_shutdown_hook)
-        
+
         self.counter = 0
         self.bridge = CvBridge()
         self.listener = tf.TransformListener()
-    
+
         self.square_size = pattern_size
         self.grid_size = (pattern_cols-1, pattern_rows-1)
 
-        self.laser_profile = Profile(axis=1, thr=180, method='pcog')        
+        self.laser_profile = Profile(axis=1, thr=180, method='pcog')
         self.camera_calibration = CameraCalibration(grid_size=self.grid_size, square_size=self.square_size)
-               
+
         cv2.namedWindow('viewer')
-        cv2.cv.SetMouseCallback('viewer', self.on_mouse, '') 
-                
+        cv2.cv.SetMouseCallback('viewer', self.on_mouse, '')
+
         rospy.spin()
-        
+
     def on_shutdown_hook(self):
         cv2.destroyWindow('viewer')
         #TODO: Fix changes in the Calibration module.
@@ -56,7 +56,7 @@ class ImageViewer():
         #print os.path.join(path, 'data', 'frame*.png')
         #calibration.find_calibration_3d(os.path.join(path, 'data', 'frame*.png'))
         #calibration.save_parameters(os.path.join(path, 'config', config_file))
-                
+
     def on_mouse(self, event, x, y, flags, params):
         if event == cv2.cv.CV_EVENT_RBUTTONDOWN:
             self.counter += 1
@@ -70,28 +70,23 @@ class ImageViewer():
                 with open(filename, 'w') as f:
                     f.write(str(transform))
                 rospy.loginfo(transform)
-                #TODO: Save pose and use it for camera pose estimation.
-                with open(filename, 'r') as f:
-                    trans, quat = eval(f.readline())
-                    rot = tf.transformations.quaternion_matrix(quat)[:3,:3]
-                    print '>>>', trans, quat, rot
             except:
                 rospy.loginfo('The transformation is not accesible.')
 
     def callback(self, data):
         try:
             self.stamp = data.header.stamp
-            self.frame = self.bridge.imgmsg_to_cv2(data)            
+            self.frame = self.bridge.imgmsg_to_cv2(data)
              #TODO: Change to work with gray images
             if data.encoding == 'mono8':
-                self.frame = cv2.cvtColor(self.frame, cv2.COLOR_GRAY2BGR) 
+                self.frame = cv2.cvtColor(self.frame, cv2.COLOR_GRAY2BGR)
             #TODO: Show chessboard and line detection.
             frame = cv2.resize(self.frame, (self.frame.shape[1]/2, self.frame.shape[0]/2))
             grid = self.camera_calibration.find_chessboard(frame)
             if grid is not None:
                 self.camera_calibration.draw_chessboard(frame, grid)
             cv2.imshow("viewer", frame)
-            key = cv2.waitKey(1)
+            cv2.waitKey(1)
         except CvBridgeError, e:
             print e
 
@@ -99,4 +94,3 @@ class ImageViewer():
 
 if __name__ == '__main__':
     ImageViewer()
-    
